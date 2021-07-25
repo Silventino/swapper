ff02171f694fc43657fcdba5bbd789fbf65cc9da
Pedro <silventino.dev@gmail.com>
2021-07-25 20:23:21 -0300

    tests

---
+++ frontend/src/algoHelper.ts
@@ -0,0 +1,53 @@
+import AlgodClient from "algosdk/dist/types/src/client/v2/algod/algod";
+
+export const waitForConfirmation = async function (
+  algodClient: AlgodClient,
+  txId: string,
+  timeout: number
+) {
+  if (algodClient == null || txId == null || timeout < 0) {
+    throw new Error("Bad arguments");
+  }
+
+  const status = await algodClient.status().do();
+  if (status === undefined) {
+    throw new Error("Unable to get node status");
+  }
+
+  const startround = status["last-round"] + 1;
+  let currentround = startround;
+
+  while (currentround < startround + timeout) {
+    const pendingInfo = await algodClient
+      .pendingTransactionInformation(txId)
+      .do();
+    if (pendingInfo !== undefined) {
+      if (
+        pendingInfo["confirmed-round"] !== null &&
+        pendingInfo["confirmed-round"] > 0
+      ) {
+        //Got the completed Transaction
+        return pendingInfo;
+      } else {
+        if (
+          pendingInfo["pool-error"] != null &&
+          pendingInfo["pool-error"].length > 0
+        ) {
+          // If there was a pool error, then the transaction has been rejected!
+          throw new Error(
+            "Transaction " +
+              txId +
+              " rejected - pool error: " +
+              pendingInfo["pool-error"]
+          );
+        }
+      }
+    }
+    await algodClient.statusAfterBlock(currentround).do();
+    currentround++;
+  }
+
+  throw new Error(
+    "Transaction " + txId + " not confirmed after " + timeout + " rounds!"
+  );
+};
